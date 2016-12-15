029c51ec509f27fd35627fc590b6402128451b53
Brady Biederman <bradybiederman@ymail.com>
2016-12-14 19:24:04 -0500

    IT=1050Final
    Well shit.

---
+++ IT-1050Final/IT-1050Final/Elevator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050Final
+{
+    class Elevator
+    {
+
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        //public constructor: 
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            this.Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        //three public methods:
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+
+            //I CANNOT get this one part to work without breaking something. Everything else works, except this.
+            //I'm out of time and I've already spent hours on this one bit. So I guess it's getting turned in like this.
+            return Occupants[0].GetWeight() + Occupants[1].GetWeight() + Occupants[2].GetWeight();
+
+
+            //if (Occupants[Occupants.Length] = 3)
+            //{
+            //    return Occupants[0].GetWeight() +
+            //      Occupants[1].GetWeight() +
+            //      Occupants[2].GetWeight();
+            //}
+            //else
+            //{
+            //    return Occupants[0].GetWeight() + Occupants[1].GetWeight();
+            //}
+
+
+        }
+
+        
+        public bool IsOverMaxCapacity()
+        {
+            return GetCurrentWeight() > MaxWeight;
+            
+        }
+
+
+    }
+}
---
+++ IT-1050Final/IT-1050Final/Passenger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050Final
+{
+    class Passenger
+    {
+
+        private string Name;
+        private double Weight;
+
+
+        //public constructor:
+        public Passenger(string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+
+        }
+
+        //public methods(2): GetName returns Name, GetWeight returns Weight;
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public double GetWeight()
+        {
+            Weight = 0;
+            return this.Weight;
+        }
+
+    }
+}
